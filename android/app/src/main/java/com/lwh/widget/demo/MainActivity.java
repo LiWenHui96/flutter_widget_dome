4222708fe25290ed4b3f62bcf409912c6855d527
LiWeNHuI <sdgrlwh@163.com>
2021-01-29 17:21:50 +0800

    init:配置ios信息； feat:Android设置透明沉浸式状态栏

--- android/app/src/main/java/com/lwh/widget/demo/MainActivity.java
+++ android/app/src/main/java/com/lwh/widget/demo/MainActivity.java
@@ -1,9 +1,13 @@
 package com.lwh.widget.demo;
 
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import com.lwh.widget.demo.utils.CommonUtils;
 
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
@@ -24,6 +28,12 @@ public class MainActivity extends FlutterActivity {
     super.onCreate(savedInstanceState);
 
     instance = this;
+
+    //设置竖屏模式
+    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+    // 沉浸式状态栏
+    CommonUtils.setStatusColor(this, Color.TRANSPARENT, false);
 
     // 初始化方法调用处理器
     MethodInvokeHandler.init();
