9c7a7026044b21d82af08297ad1b31b1f6a328e5
LiWeNHuI <sdgrlwh@163.com>
2021-01-29 15:58:36 +0800

    feat:配置Toast插件；

---
+++ lib/util/common_util.dart
@@ -0,0 +1,18 @@
+import 'package:fluttertoast/fluttertoast.dart';
+
+/// @Author: LWH
+/// @Host: liwenhui
+/// @Date: 2021/01/29 3:01 PM
+/// @Describe: 
+
+class CommonUtils {
+  /// Toast
+  static void showToast(String msg, {bool isLong = false}) {
+    Fluttertoast.showToast(
+        msg: msg,
+        toastLength: isLong ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: isLong ? 2 : 1
+    );
+  }
+}
---
+++ lib/util/util.dart
@@ -0,0 +1,6 @@
+/// @Author: LWH
+/// @Host: liwenhui
+/// @Date: 2021/01/29 3:10 PM
+/// @Describe: 
+
+export "common_util.dart";
