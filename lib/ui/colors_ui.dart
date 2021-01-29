bdb20c7463aa49bb5ccad582243223738acbd9ac
LiWeNHuI <sdgrlwh@163.com>
2021-01-29 16:00:15 +0800

    feat:完成颜色UI配置

---
+++ lib/ui/colors_ui.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+/// @Author: LWH
+/// @Host: liwenhui
+/// @Date: 2021/01/29 3:20 PM
+/// @Describe: 
+
+class ColorsUi {
+  ColorsUi._();
+
+  // 主题色
+  static const Color themeColor = const Color(0xFF2F90F2);
+  // 字体黑
+  static const Color text_dark = Color(0xFF333333);
+}
---
+++ lib/ui/ui.dart
@@ -0,0 +1,6 @@
+/// @Author: LWH
+/// @Host: liwenhui
+/// @Date: 2021/01/29 3:20 PM
+/// @Describe: 
+
+export "colors_ui.dart";
