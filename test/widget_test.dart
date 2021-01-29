6bd3e9c6c6adcb93ef620830fd8eb20977d4b52d
LiWeNHuI <sdgrlwh@163.com>
2021-01-29 15:57:12 +0800

    init:项目配置

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_widget_demo/page/page_home.dart';
+import 'package:flutter_widget_demo/page/page_main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
